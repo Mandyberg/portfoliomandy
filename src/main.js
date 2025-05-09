8d4b340899e702bd8403f16fefef2d3591fa92f8
Mandyberg <mandyvdb1@live.nl>
2025-05-09 07:26:29 +0200

    Update main.js
    //e.preventDefault as comment

--- src/main.js
+++ src/main.js
@@ -192,13 +192,13 @@ window.addEventListener("mousemove", (e) => {
 });
 
 window.addEventListener("touchstart", (e) => {
-  e.preventDefault();
+  //e.preventDefault();
   pointer.x = (e.touches[0].clientX / window.innerWidth) * 2 - 1;
   pointer.y = - (e.touches[0].clientY / window.innerHeight) * 2 + 1;
 }, { passive: false });
 
 window.addEventListener("touchend", (e) => {
-  e.preventDefault();
+  //e.preventDefault();
   handleRaycasterInteraction();
 }, { passive: false });
 
